311533ddd69ab1bc6f89011dd494690dbef936f0
Gabriel Vidigal <gabrielcvfcvf@gmail.com>
2023-08-16 16:58:53 -0300

    feat: create categoryService

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,20 +1,23 @@
-import { ProductEntity } from "src/product/entities/product.entity";
-import { Column, CreateDateColumn, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-
+import { ProductEntity } from '../../product/entities/product.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+@Entity({ name: 'category' })
 export class CategoryEntity {
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+  @Column({ name: 'name', nullable: false })
+  name: string;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 
-    @PrimaryGeneratedColumn('rowid')
-    id: number;
-
-    @Column({name: 'name', nullable: false})
-    name: string;
-
-    @CreateDateColumn({ name: 'created_at'})
-    createdAt: Date;
-
-    @UpdateDateColumn({ name: 'updated_at'})
-    updatedAt: Date;
-
-    @OneToMany(() => ProductEntity, (product: ProductEntity) => product.category)
-    procuts?: ProductEntity;
+  @OneToMany(() => ProductEntity, (product: ProductEntity) => product.category)
+  products?: ProductEntity;
 }
