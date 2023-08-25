07e1bb72e1ca16e9925ae7416c1214c14c178f83
Gabriel Vidigal <gabrielcvfcvf@gmail.com>
2023-08-25 14:28:10 -0300

    feat: create cart of user

--- src/cart/entities/cart.entity.ts
+++ src/cart/entities/cart.entity.ts
@@ -1,20 +1,27 @@
-import { CartProductEntity } from "src/cart-product/entities/cart-product.entity";
-import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-
-@Entity({ name: 'cart'})
+import { CartProductEntity } from 'src/cart-product/entities/cart-product.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+@Entity({ name: 'cart' })
 export class CartEntity {
-    @PrimaryGeneratedColumn('rowid')
-    id: number;
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+  @Column({ name: 'user_id', nullable: false })
+  userId: number;
 
-    @Column({ name: 'user_id', nullable: false })
-    userId: string;
+  @Column({ name: 'active', nullable: false })
+  active: boolean;
 
-    @CreateDateColumn({ name: 'created_at' })
-    createdAt: Date;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
 
-    @UpdateDateColumn({ name: 'updated_at' })
-    updatedAt: Date;
-
-    @OneToMany(() => CartProductEntity, (cartProduct) => cartProduct.cart)
-    cartProduct?: CartProductEntity[];
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+  @OneToMany(() => CartProductEntity, (cartProduct) => cartProduct.cart)
+  cartProduct?: CartProductEntity[];
 }
---
+++ src/migration/1692973864852-alter-table-cart.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AlterTableCart1692973864852 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+                ALTER TABLE cart ADD active boolean NOT NULL;
+            `);
+      }
+    
+      public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+                ALTER TABLE cart drop active;
+            `);
+      }
+    }
