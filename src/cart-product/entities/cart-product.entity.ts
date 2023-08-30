d4d72c762f97cb56846c7e0f581637134a77abbe
Gabriel Vidigal <gabrielcvfcvf@gmail.com>
2023-08-30 12:43:14 -0300

    feat: insert product in cart

--- src/cart-product/entities/cart-product.entity.ts
+++ src/cart-product/entities/cart-product.entity.ts
@@ -2,7 +2,7 @@ import { CartEntity } from "src/cart/entities/cart.entity";
 import { ProductEntity } from "src/product/entities/product.entity";
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity({ name: 'product' })
+@Entity({ name: 'cart_product' })
 export class CartProductEntity {
   @PrimaryGeneratedColumn('rowid')
   id: number;
