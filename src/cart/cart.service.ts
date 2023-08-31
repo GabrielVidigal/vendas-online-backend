c790989634e4763a438423423aa5dbb6c9025430
Gabriel Vidigal <gabrielcvfcvf@gmail.com>
2023-08-31 20:17:50 -0300

    feat: verify productId and create cart dto

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -13,11 +13,18 @@ export class CartService {
         private readonly cartProductService: CartProductService
     ) {}
 
-    async verifyActiveCart(userId: number): Promise<CartEntity> {
+    async findCartByUserId(userId: number, isRelations?: boolean): Promise<CartEntity> {
+        const relations = isRelations ?  {
+          cartProduct: {
+            product: true,
+          }
+        } : undefined
         const cart = await this.cartRepository.findOne({
           where: {
             userId,
+            active: true,
           },
+          relations
         });
     
         if (!cart) {
@@ -26,6 +33,7 @@ export class CartService {
     
         return cart;
       }
+
     
       async createCart(userId: number): Promise<CartEntity> {
         return this.cartRepository.save({
@@ -38,12 +46,12 @@ export class CartService {
         insertCartDTO: InsertCartDTO,
         userId: number,
       ): Promise<CartEntity> {
-        const cart = await this.verifyActiveCart(userId).catch(async () => {
+        const cart = await this.findCartByUserId(userId).catch(async () => {
           return this.createCart(userId);
         });
 
         await this.cartProductService.insertProductInCart(insertCartDTO, cart)
     
-        return cart;
+        return this.findCartByUserId(userId, true);
       }
     }
