185e6f8bdb484c3aeb575185fc6a9f4e828908ac
Gabriel Vidigal <gabrielcvfcvf@gmail.com>
2023-09-27 11:46:04 -0300

    feat:create unit test in payment and order-product

--- src/cart/__mocks__/update-cart.mock.ts
+++ src/cart/__mocks__/update-cart.mock.ts
@@ -2,6 +2,6 @@ import { productMock } from '../../product/__mocks__/product.mock';
 import { UpdateCartDTO } from '../dtos/update-cart.dto';
 
 export const updateCartMock: UpdateCartDTO = {
-  amount: 53445,
+  amount: 54638,
   productId: productMock.id,
 };
--- src/payment/__mocks__/payment.mock.ts
+++ src/payment/__mocks__/payment.mock.ts
@@ -1,13 +1,13 @@
-import { PaymentType } from "../../payment-status/enums/payment-type.enum";
-import { PaymentEntity } from "../entities/payment.entity";
+import { PaymentType } from '../../payment-status/enums/payment-type.enum';
+import { PaymentEntity } from '../entities/payment.entity';
 
 export const paymentMock: PaymentEntity = {
-    createdAt: new Date(),
-    discount: 432,
-    finalPrice: 6345634.45,
-    id: 5432523,
-    price: 5432112.4,
-    statusId: PaymentType.Done,
-    updatedAt: new Date(),
-    type: '',
-}
+  createdAt: new Date(),
+  discount: 432,
+  finalPrice: 64365.4,
+  id: 52358,
+  price: 32532.0,
+  statusId: PaymentType.Done,
+  updatedAt: new Date(),
+  type: '',
+};
