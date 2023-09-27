8fa76a18887df6204f705b08e105647b97adfd42
Gabriel Vidigal <gabrielcvfcvf@gmail.com>
2023-09-27 10:48:51 -0300

    feat: insert unit test in paymentService

--- src/payment/__mocks__/payment-pix.mock.ts
+++ src/payment/__mocks__/payment-pix.mock.ts
@@ -1,8 +1,9 @@
+import { createOrderPixMock } from "../../order/__mocks__/create-order.mock";
 import { PaymentPixEntity } from "../entities/payment-pix.entity";
 import { paymentMock } from "./payment.mock"
 
 export const paymentPixMock: PaymentPixEntity = {
     ...paymentMock,
     code: 'fadsfas',
-    datePayment: new Date(),
+    datePayment: new Date('2022-01-01'),
 }
