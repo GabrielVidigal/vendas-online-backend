7260c5aa97a43d63b5f634dad49ed92bbc3a9807
Gabriel Vidigal <gabrielcvfcvf@gmail.com>
2023-09-29 09:42:55 -0300

    feat: insert amount product in category

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -7,22 +7,38 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CategoryEntity } from './entities/category.entity';
 import { CreateCategory } from './dtos/create-category.dto';
+import { ProductService } from 'src/product/product.service';
+import { ReturnCategory } from './dtos/return-category.dto';
 
 @Injectable()
 export class CategoryService {
   constructor(
     @InjectRepository(CategoryEntity)
     private readonly categoryRepository: Repository<CategoryEntity>,
+
+    private readonly productService: ProductService,
   ) {}
 
-  async findAllCategories(): Promise<CategoryEntity[]> {
+  findAmountCategoryInProducts(category: CategoryEntity, countList: any): number{
+    const count = countList.find((itemCount) => itemCount.category_id === category.id)
+
+    if (count) {
+      return count.total;
+    }
+    return 0;
+  }
+
+  async findAllCategories(): Promise<ReturnCategory[]> {
     const categories = await this.categoryRepository.find();
+
+    const count = await this.productService.countProductsByCategoryId();
+
 
     if (!categories || categories.length === 0) {
       throw new NotFoundException('Categories empty');
     }
 
-    return categories;
+    return categories.map((category) => new ReturnCategory(category, this.findAmountCategoryInProducts(category, count)));
   }
 
   async findCategoryById(categoryId: number): Promise<CategoryEntity> {
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,16 +1,19 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, forwardRef, Inject  } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProductEntity } from './entities/product.entity';
 import { DeleteResult, In, Repository } from 'typeorm';
 import { CreateProductDTO } from './dtos/create-product.dto';
 import { CategoryService } from '../category/category.service';
 import { UpdateProductDTO } from './dtos/update-product.dto';
+import { CountProduct } from './dtos/count-product.dto';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(ProductEntity)
     private readonly productRepository: Repository<ProductEntity>,
+
+    @Inject(forwardRef(() => CategoryService))
     private readonly categoryService: CategoryService,
   ) {}
 
@@ -76,5 +79,12 @@ export class ProductService {
       ...product,
       ...updateProduct,
     });
+  }
+
+  async countProductsByCategoryId(): Promise<CountProduct[]> {
+    return this.productRepository.createQueryBuilder('product')
+      .select('product.category_id, COUNT(*) as total ')
+      .groupBy('product.category_id')
+      .getRawMany();
   }
 }
