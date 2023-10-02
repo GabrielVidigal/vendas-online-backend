d34afd107a6edd63d6fbace374119cb6ec666170
Gabriel Vidigal <gabrielcvfcvf@gmail.com>
2023-10-02 10:13:05 -0300

    feat: insert return and unit test in findOrderById

--- src/order/dtos/return-order.dto.ts
+++ src/order/dtos/return-order.dto.ts
@@ -1,20 +1,26 @@
-import { OrderProductEntity } from '../../order-product/entities/order-product.entity';
 import { ReturnAddressDto } from '../../address/dtos/returnAddress.dto';
 import { ReturnPaymentDTO } from '../../payment/dtos/return-payment.dto';
 import { ReturnUserDto } from '../../user/dtos/returnUser.dto';
 import { OrderEntity } from '../entities/order.entity';
+import { ReturnOrderProdcutDTO } from '../../order-product/dtos/return-order-product.dto';
 
 export class ReturnOrderDTO {
   id: number;
   date: string;
+  userId: number;
+  addressId: number;
+  paymentId: number;
   user?: ReturnUserDto;
   address?: ReturnAddressDto;
   payment?: ReturnPaymentDTO;
-  ordersProduct?: OrderProductEntity[];
+  ordersProduct?: ReturnOrderProdcutDTO[];
 
   constructor(order: OrderEntity) {
     this.id = order.id;
     this.date = order.date.toString();
+    this.userId = order.userId;
+    this.addressId = order.addressId;
+    this.paymentId = order.paymentId;
     this.user = order.user ? new ReturnUserDto(order.user) : undefined;
     this.address = order.address
       ? new ReturnAddressDto(order.address)
@@ -22,6 +28,10 @@ export class ReturnOrderDTO {
     this.payment = order.payment
       ? new ReturnPaymentDTO(order.payment)
       : undefined;
-    this.ordersProduct = order.ordersProduct;
+    this.ordersProduct = order.ordersProduct 
+    ? order.ordersProduct.map(
+      (orderProduct) => new ReturnOrderProdcutDTO(orderProduct)
+      ) 
+      : undefined
   }
 }
