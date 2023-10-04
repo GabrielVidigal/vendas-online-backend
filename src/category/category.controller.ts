2a45deabe8aac2f513c74fd26e66dbcafc77ab84
Gabriel Vidigal <gabrielcvfcvf@gmail.com>
2023-10-04 11:23:01 -0300

    feat: insert endpoint delete category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   Get,
   Post,
+  Param,
+  Delete,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -12,6 +14,7 @@ import { CategoryService } from './category.service';
 import { ReturnCategory } from './dtos/return-category.dto';
 import { CreateCategory } from './dtos/create-category.dto';
 import { CategoryEntity } from './entities/category.entity';
+import { DeleteResult } from 'typeorm';
 
 @Roles(UserType.Admin, UserType.Root, UserType.User)
 @Controller('category')
@@ -30,5 +33,11 @@ export class CategoryController {
     @Body() createCategory: CreateCategory,
   ): Promise<CategoryEntity> {
     return this.categoryService.createCategory(createCategory);
+  }
+
+  @Roles(UserType.Admin, UserType.Root)
+  @Delete(':categoryId')
+  async deleteCategory(@Param('categoryId') categoryId: number): Promise<DeleteResult> {
+    return this.categoryService.deleteCategory(categoryId)
   }
 }
